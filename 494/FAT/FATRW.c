3025837ffa8cac58b0eda14d644d74a791ce7a47
MungShu <mungshushen@gmail.com>
2024-03-12 16:22:49 -0400

    fat

--- 494/FAT/FATRW.c
+++ 494/FAT/FATRW.c
@@ -11,15 +11,18 @@
 #define SECTOR_MAX_LINKS (512)
 
 char BUF[1024];
-//unsigned short LINKS [512];
+
 typedef struct disk{
    void *dp = NULL;
    int written = -1;
    int read = -1;
    int changed = -1;
 } disk;
-//unsigned short read_short(int link, unsigned short LINKS[]){
-   //}
+void EXPORT(int start_block, FILE *fp){
+   (void) start_block;
+   (void) fp;
+
+}
 int main(int argc, char** argv){
    struct stat buf;
    char* file = (char*) malloc(sizeof(char) * 50);
@@ -42,14 +45,11 @@ int main(int argc, char** argv){
       strcpy(file, argv[4]);
       startblock = atoi(argv[3]);
    }
-   
+
    fp = fopen(file, "r");
    fseek(fp, 0, SEEK_END);
    fsize = ftell(fp);
    fseek(fp, 0, SEEK_SET);
-   //char finput[1];
-   //int buf_size = fread(finput, sizeof(char), 
-   
    p = jdisk_attach(diskfile);
    if (p == NULL){
       fprintf(stderr, "###\n");
@@ -57,45 +57,72 @@ int main(int argc, char** argv){
    }
    dsize = jdisk_size(p);
    dsectors = dsize / SECTOR_SIZE;
-  
+
    FAT_SIZE = ceil((double) dsectors / SECTOR_MAX_LINKS);
    unsigned short LINKS[FAT_SIZE * SECTOR_MAX_LINKS];
    jdisk_read(p, 0, LINKS);
    disk Disks[dsectors];   
-
-   
    if (dsize <= fsize){
       fprintf(stderr, "%s is too big for the disk (%ld vs %d)\n", file, fsize, dsize - SECTOR_SIZE);
       exit(1);
    }
-   
    int k = 0;
    int entry;
-   int first_free = 0;
+   int first_free = LINKS[0];
    int free_sector_start = FAT_SIZE; 
+   int ss;
+   unsigned short o;
+   if (fsize % 1024 == 0) ss = 0;
+   else if (fsize % 1024 == 1023) {
+      ss = 1;
+      o = 0xff;
+   }
+   else if (fsize % 1024 < 1023){
+      ss = 2;
+      o = fsize % 1024;
+   }
+   if (first_free == 0){
+      printf("#F\n");
+   }
+   while (first_free != 0){
+      first_free = LINKS[first_free]; //?
+   }
    fsectors = ceil((double) fsize / SECTOR_SIZE);
+   if ( argc == 4){
    int i;
    for (i = 0; i < fsectors; i++){
-      fseek(fp, i * SECTOR_SIZE, SEEK_SET); 
+
       k += fread(BUF, sizeof(char), SECTOR_SIZE, fp);
-      jdisk_write(p, i + FAT_SIZE, BUF);
+      if (i == fsectors-1){
+         
+         if (ss == 1) memcpy(BUF + 1023, &o, sizeof(char));
+         else if (ss == 2) memcpy(BUF + 1022, &o, sizeof(short));
+      }
+      jdisk_write(p, LINKS[first_free] + FAT_SIZE - 1, BUF); // ???
       first_free = LINKS[first_free];
       free_sector_start ++;
    }
-
-   LINKS[0] = LINKS[first_free];
-   LINKS[first_free] = 0;
+//   printf("%d %d\n", first_free, LINKS[first_free]);
+   LINKS[0] = LINKS[first_free]; //?
+   switch (ss){
+      case 0:
+         LINKS[first_free] = 0;
+         break;
+      case 1:
+      case 2:
+         LINKS[first_free] = first_free;
+         break; 
+   }
 
    jdisk_write(p, 0, LINKS);
    fclose(fp);
-   free_sector_start ++;
-   
+
+}
+else if (argc == 5){
+  
+}
    printf("New file starts at sector %d\n", free_sector_start);
    printf("Reads: %ld\n", jdisk_reads(p));
    printf("Writes: %ld\n", jdisk_writes(p));
-
-
-   
-
    return 0;
 }
